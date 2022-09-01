6ee395900a452123c0b0340266e13aeb07e1c022
FumoNeko <28814987+FumoNeko@users.noreply.github.com>
2022-09-01 00:58:20 -0700

    fixed data.h doubles to floats

--- dmgcalc/data.h
+++ dmgcalc/data.h
@@ -1,194 +1,194 @@
 struct {
-    float baseEntityDamage = 6;
-    float baseBlockDamage = 21.5;
+    float baseEntityDamage = 6f;
+    float baseBlockDamage = 21.5f;
 } StoneAxe;
 
 struct {
-    float baseEntityDamage = 8;
-    float baseBlockDamage = 22.6;
+    float baseEntityDamage = 8f;
+    float baseBlockDamage = 22.6f;
 } TazasStoneAxe;
 
 struct {
-    float baseEntityDamage = 12;
-    float baseBlockDamage = 22;
+    float baseEntityDamage = 12f;
+    float baseBlockDamage = 22f;
 } ClawHammer;
 
 struct {
-    float baseEntityDamage = 15.1;
-    float baseBlockDamage = 47.0;
+    float baseEntityDamage = 15.1f;
+    float baseBlockDamage = 47.0f;
 } IronFireAxe;
 
 struct {
-    float baseEntityDamage = 28.9;
-    float baseBlockDamage = 86.2;
+    float baseEntityDamage = 28.9f;
+    float baseBlockDamage = 86.2f;
 } SteelAxe;
 
 struct {
-    float baseEntityDamage = 11.2;
-    float baseBlockDamage = 35.5;
+    float baseEntityDamage = 11.2f;
+    float baseBlockDamage = 35.5f;
 } IronPickaxe;
 
 struct {
-    float baseEntityDamage = 23.2;
-    float baseBlockDamage = 62.8;
+    float baseEntityDamage = 23.2f;
+    float baseBlockDamage = 62.8f;
 } StelPickaxe;
 
 struct {
-    float baseEntityDamage = 9.6;
-    float baseBlockDamage = 38.4;
+    float baseEntityDamage = 9.6f;
+    float baseBlockDamage = 38.4f;
 } StoneShovel;
 
 struct {
-    float baseEntityDamage = 11.6;
-    float baseBlockDamage = 51;
+    float baseEntityDamage = 11.6f;
+    float baseBlockDamage = 51f;
 } IronShovel;
 
 struct {
-    float baseEntityDamage = 17.4;
-    float baseBlockDamage = 69;
+    float baseEntityDamage = 17.4f;
+    float baseBlockDamage = 69f;
 } SteelShovel;
 
 struct {
-    float baseEntityDamage = 6;
-    float baseBlockDamage = 24.3;
+    float baseEntityDamage = 6f;
+    float baseBlockDamage = 24.3f;
 } Chainsaw;
 
 struct {
-    float baseEntityDamage = 4.2;
-    float baseBlockDamage = 20.7;
+    float baseEntityDamage = 4.2f;
+    float baseBlockDamage = 20.7f;
 } Auger;
 
 struct {
-    float baseEntityDamage = 16.5;
-    float baseBlockDamage = 38.5;
+    float baseEntityDamage = 16.5f;
+    float baseBlockDamage = 38.5f;
 } Wrench;
 
 struct {
-    float baseEntityDamage = 16.5;
-    float baseBlockDamage = 48.5;
+    float baseEntityDamage = 16.5f;
+    float baseBlockDamage = 48.5f;
 } Ratchet;
 
 struct {
-    float baseEntityDamage = 16.5;
-    float baseBlockDamage = 58;
+    float baseEntityDamage = 16.5f;
+    float baseBlockDamage = 58f;
 } ImpactDriver;
 
 struct {
-    float baseEntityDamage = 5.1;
-    float baseBlockDamage = 4;
+    float baseEntityDamage = 5.1f;
+    float baseBlockDamage = 4f;
 } BoneKnife;
 
 struct {
-    float baseEntityDamage = 6.1;
-    float baseBlockDamage = 5.3;
+    float baseEntityDamage = 6.1f;
+    float baseBlockDamage = 5.3f;
 } HuntingKnife;
 
 struct {
-    float baseEntityDamage = 6.6;
-    float baseBlockDamage = 5.5;
+    float baseEntityDamage = 6.6f;
+    float baseBlockDamage = 5.5f;
 } CandyKnife;
 
 struct {
-    float baseEntityDamage = 19.8;
-    float baseBlockDamage = 21;
+    float baseEntityDamage = 19.8f;
+    float baseBlockDamage = 21f;
 } Machete;
 
 struct {
-    float baseEntityDamage = 13.8;
-    float baseBlockDamage = 11.6;
+    float baseEntityDamage = 13.8f;
+    float baseBlockDamage = 11.6f;
 } WoodenClub;
 
 struct {
-    float baseEntityDamage = 17.4;
-    float baseBlockDamage = 14.6;
+    float baseEntityDamage = 17.4f;
+    float baseBlockDamage = 14.6f;
 } BaseballBat;
 
 struct {
-    float baseEntityDamage = 19.2;
-    float baseBlockDamage = 16;
+    float baseEntityDamage = 19.2f;
+    float baseBlockDamage = 16f;
 } CandyClub;
 
 struct {
-    float baseEntityDamage = 26.2;
-    float baseBlockDamage = 22.1;
+    float baseEntityDamage = 26.2f;
+    float baseBlockDamage = 22.1f;
 } SteelClub;
 
 struct {
-    float baseEntityDamage = 13.8;
-    float baseBlockDamage = 11.6;
+    float baseEntityDamage = 13.8f;
+    float baseBlockDamage = 11.6f;
 } PipeBaton;
 
 struct {
-    float baseEntityDamage = 10.8;
-    float baseBlockDamage = 6;
+    float baseEntityDamage = 10.8f;
+    float baseBlockDamage = 6f;
 } StunBaton;
 
 struct {
-    float baseEntityDamage = 10.8;
-    float baseBlockDamage = 6;
+    float baseEntityDamage = 10.8f;
+    float baseBlockDamage = 6f;
 } PlasmaBaton;
 
 struct {
-    float baseEntityDamage = 10.3;
-    float baseBlockDamage = 5;
+    float baseEntityDamage = 10.3f;
+    float baseBlockDamage = 5f;
 } StoneSpear;
 
 struct {
-    float baseEntityDamage = 12.4;
-    float baseBlockDamage = 7;
+    float baseEntityDamage = 12.4f;
+    float baseBlockDamage = 7f;
 } IronSpear;
 
 struct {
-    float baseEntityDamage = 18.6;
-    float baseBlockDamage = 9;
+    float baseEntityDamage = 18.6f;
+    float baseBlockDamage = 9f;
 } SteelSpear;
 
 struct {
-    float baseEntityDamage = 17.5;
-    float baseBlockDamage = 27.2;
+    float baseEntityDamage = 17.5f;
+    float baseBlockDamage = 27.2f;
 } StoneSledgehammer;
 
 struct {
-    float baseEntityDamage = 30.8;
-    float baseBlockDamage = 58.3;
+    float baseEntityDamage = 30.8f;
+    float baseBlockDamage = 58.3f;
 } IronSledgehammer;
 
 struct {
-    float baseEntityDamage = 46.2;
-    float baseBlockDamage = 85;
+    float baseEntityDamage = 46.2f;
+    float baseBlockDamage = 85f;
 } SteelSledgehammer;
 
 struct {
-    float baseEntityDamage = 6.2;
-    float baseBlockDamage = 2.2;
+    float baseEntityDamage = 6.2f;
+    float baseBlockDamage = 2.2f;
 } LeatherKnuckles;
 
 struct {
-    float baseEntityDamage = 9.1;
-    float baseBlockDamage = 3;
+    float baseEntityDamage = 9.1f;
+    float baseBlockDamage = 3f;
 } IronKnuckles;
 
 struct {
-    float baseEntityDamage = 13.6;
-    float baseBlockDamage = 5.2;
+    float baseEntityDamage = 13.6f;
+    float baseBlockDamage = 5.2f;
 } SteelKnuckles;
 
 struct {
-    float baseEntityDamage = 13.2;
-    float baseBlockDamage = 26;
+    float baseEntityDamage = 13.2f;
+    float baseBlockDamage = 26f;
 } IronHoe;
 
 struct {
-    float baseEntityDamage = 7.2;
-    float baseBlockDamage = 6;
+    float baseEntityDamage = 7.2f;
+    float baseBlockDamage = 6f;
 } Flashlight;
 
 struct {
-    float baseEntityDamage = 10.2;
-    float baseBlockDamage = 7;
+    float baseEntityDamage = 10.2f;
+    float baseBlockDamage = 7f;
 } Torch;
 
 struct {
-    float baseEntityDamage = 11.5;
-    float baseBlockDamage = 17;
+    float baseEntityDamage = 11.5f;
+    float baseBlockDamage = 17f;
 } RoboticSledge;
