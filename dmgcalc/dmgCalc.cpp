712ebb7c6801f84e21c264b082b7236fffdb83a9
FumoNeko <28814987+FumoNeko@users.noreply.github.com>
2022-09-02 16:10:59 -0700

    fixed array types

--- dmgcalc/dmgCalc.cpp
+++ dmgcalc/dmgCalc.cpp
@@ -10,13 +10,13 @@ int main() {
     bool menuState = true;
     if (menuState == true){
         returnCode = termGui("main");
-        optionsMain[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-        optionsWeapon[39] = {}; //100-138
+        int optionsMain[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+        int optionsWeapon[39] = {}; // {100, 101, ... 138}
         for (int i = 0; i < 39; i++) {
             optionsWeapon[i] = 100 + i;
         }
-        optionsQuality[7] = {200, 201, 202, 203, 204, 205, 206};
-        optionsPerkLevel[] = {300, 301, 302, 303, 304, 305, 306};
+        int optionsQuality[7] = {200, 201, 202, 203, 204, 205, 206};
+        int optionsPerkLevel[7] = {300, 301, 302, 303, 304, 305, 306};
 
     }
 
